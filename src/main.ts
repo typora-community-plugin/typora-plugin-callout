8fa388b3d43cefd6cfc8d608757e96ffa128fe3a
plylrnsdy <plylrnsdy@163.com>
2024-09-05 13:20:02 +0800

    refactor: use `registerMarkdownPostProcessor()` instead of `activeEditor.on('edit')`

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import './style.scss'
-import { Plugin } from '@typora-community-plugin/core'
+import { HtmlPostProcessor, Plugin } from '@typora-community-plugin/core'
 
 
 export default class extends Plugin {
 
   onload() {
     this.register(
-      this.app.workspace.on('file:open', () => render().map(fold))
+      this.app.workspace.on('file:open', () =>
+        Array.from(document.querySelectorAll('#write [mdtype="blockquote"] > :first-child'))
+          .map(render)
+          .map(fold))
     )
-    this.register(
-      this.app.workspace.activeEditor.on('edit', render)
+
+    this.registerMarkdownPostProcessor(
+      HtmlPostProcessor.from({
+        selector: '#write [mdtype="blockquote"] > :first-child',
+        process: render,
+      })
     )
 
     const write = document.getElementById('write')!
@@ -20,22 +27,19 @@ export default class extends Plugin {
   }
 }
 
-function render() {
-  return Array.from(document.querySelectorAll('#write [mdtype="blockquote"] > :first-child'))
-    .map((p: HTMLElement) => {
-      const blockquote = p.parentElement!
-      const [, type, foldStatus] = p.textContent!.match(/^\[!(\w+)\]([+-]?)/) ?? []
+function render(p: HTMLElement) {
+  const blockquote = p.parentElement!
+  const [, type, foldStatus] = p.textContent!.match(/^\[!(\w+)\]([+-]?)/) ?? []
 
-      if (type) {
-        blockquote.classList.add('typ-callout')
-        blockquote.dataset.callout = p.dataset.callout = type.toLowerCase()
-      }
-      else {
-        blockquote.classList.remove('typ-callout')
-      }
+  if (type) {
+    blockquote.classList.add('typ-callout')
+    blockquote.dataset.callout = p.dataset.callout = type.toLowerCase()
+  }
+  else {
+    blockquote.classList.remove('typ-callout')
+  }
 
-      return [blockquote, foldStatus === '-']
-    })
+  return [blockquote, foldStatus === '-']
 }
 
 function fold([blockquote, isFolded]: [HTMLElement, boolean]) {
